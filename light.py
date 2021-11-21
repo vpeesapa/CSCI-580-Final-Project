0e749094db56de52362a2913ed113d4cda3ee54c
Varun Peesapati <peesapativarun99@gmail.com>
2021-11-20 18:41:27 -0800

    Added some comments

--- light.py
+++ light.py
@@ -1,6 +1,9 @@
 from color import Color
 
+# Class that defines a point-light source
 class Light:
+
+	# Parametrised constructor that defines the light source
 	def __init__(self,position,color = Color.toRGB("#FFFFFF")):
 		self.position = position
 		self.color = color
