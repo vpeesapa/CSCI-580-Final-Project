47c02b81d0adc9eace2182260f93a8f3938075b0
Varun Peesapati <peesapativarun99@gmail.com>
2021-11-22 13:15:33 -0800

    Modified color.py to take in color as a hex string and convert it into an 8-bit value

--- color.py
+++ color.py
@@ -2,13 +2,12 @@ from vector import Vector
 
 # Class that stores the color
 class Color(Vector):
-	pass
 
-	# Function that converts a color from hexadecimal string to r,g, and b values
-	# @classMethod
-	# def toRGB(cls,color = "#000000"):
-	# 	red = int(color[1:3],16) / 255.0
-	# 	blue = int(color[3:5],16) / 255.0
-	# 	green = int(color[5:7],16) / 255.0
+	# Function that converts a color from hexadecimal string to r, g, and b values
+	@classmethod
+	def toRGB(cls,color = "#000000"):
+		red = int(color[1:3],16) / 255.0
+		blue = int(color[3:5],16) / 255.0
+		green = int(color[5:7],16) / 255.0
 
-	# 	return cls(red,blue,green)
+		return cls(red,blue,green)
