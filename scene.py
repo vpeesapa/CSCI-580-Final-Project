d017a6398856cd19edfd3d661b4935cf89b6221f
Varun Peesapati <peesapativarun99@gmail.com>
2021-11-22 16:15:50 -0800

    Updated scene to include lights

--- scene.py
+++ scene.py
@@ -2,8 +2,9 @@
 class Scene:
 
 	# Parametrised constructor to set up the scene
-	def __init__(self,camera,objects,width,height):
+	def __init__(self,camera,objects,lights,width,height):
 		self.camera = camera
 		self.objects = objects
+		self.lights = lights
 		self.width = width
 		self.height = height
