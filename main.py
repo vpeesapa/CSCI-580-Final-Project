216e5d5b0d6ebfe96ac8af5c13bf32b6d82ee1e7
Varun Peesapati <peesapativarun99@gmail.com>
2021-11-24 20:02:03 -0800

    Updated the main script for the current workflow

--- main.py
+++ main.py
@@ -7,38 +7,29 @@ Done by:
 * Venkatesh Vayachal
 '''
 import sys
+import importlib
+import os
 
-from color import Color
-from vector import Vector
-from point import Point
-from sphere import Sphere
 from scene import Scene
 from renderer import Renderer
-from light import Light
-from material import Material
 
 def main():
 	# Checking if the file name is passed as an argument
 	if len(sys.argv) != 2:
-		print("Correct usage: py main.py <filename>.ppm")
+		print("Correct usage: py main.py <setup_script>")
 		sys.exit(1)
 
-	output_file = sys.argv[1]
+	path_to_setup = sys.argv[1]
+	mod = importlib.import_module(path_to_setup)
 
-	# Setting up the scene and the renderer
-	width = 320
-	height = 200
-	camera = Vector(0,0,-1)
-	objects = [Sphere(Point(0,0,0),0.5,Material(Color.toRGB("#FF0000")))]
-	lights = [Light(Point(1.5,-0.5,-10.0),Color.toRGB("#FFFFFF"))]
-	scene = Scene(camera,objects,lights,width,height)
+	# Rendering the scene
+	scene = Scene(mod.camera,mod.objects,mod.lights,mod.width,mod.height)
 	renderer = Renderer()
+	image = renderer.render(scene)
 
-	# Defining the image that has been rendered
-	img = renderer.render(scene)
-
-	with open(output_file,"w") as img_file:
-		img.write_ppm(img_file)
+	os.chdir(os.path.dirname(os.path.abspath(mod.__file__)))
+	with open(mod.rendered_image,"w") as img_file:
+		image.write_ppm(img_file)
 
 if __name__ == "__main__":
 	main()
